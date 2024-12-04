04166823e67b33b57314114fa41dbb4445d91b64
Kevin Huang <hkevinbiz@gmail.com>
2024-12-03 23:17:38 -0500

    Completed Day 3 Problem

---
+++ Day3/day3.py
@@ -0,0 +1,13 @@
+import json
+import re
+
+with open('Day3/day3.json', 'r') as input_text:
+  input_text = json.loads(input_text.read())
+
+matches = re.findall(r'mul\((\d+),(\d+)\)', input_text)
+
+total_sum = 0
+for a, b in matches:
+  total_sum += int(a) * int(b)
+  
+print(total_sum)
